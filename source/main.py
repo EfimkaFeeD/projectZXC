7f442450c2aadc06cb634b60ef5f661b9e2e45d9
gergy <gergy2k07@gmail.com>
2022-12-09 22:22:30 +0300

    add scroll of song list

--- source/main.py
+++ source/main.py
@@ -6,7 +6,6 @@ from pygame_widgets.dropdown import Dropdown
 from pygame_widgets.widget import WidgetHandler
 from screeninfo import get_monitors
 
-
 # Надо для определния разрешения по неясным причинам
 get_monitors()
 
@@ -60,11 +59,11 @@ class Menu:
         self.menu_song.play()
         self.song_list = [arg[1] for arg in os.walk('songs')][0]
         self.level_name = ''
-        self.spin = 0
         self.menu_image = pygame.image.load('materials//menu_bg.jpg')
         self.menu_image = pygame.transform.smoothscale(self.menu_image, (screen_width, screen_height))
         self.buttons = []
         self.song_name = ''
+        self.buttons_scroll = 0
         self.buttons_font = pygame.font.Font('materials\\Press Start 2P.ttf', int(15 * (screen_width / 1920)))
         self.buttons_font_color = (255, 255, 255)
         self.buttonArray = self.generate_song_button_array()
@@ -72,41 +71,43 @@ class Menu:
         self.difficulty_dropdown_menu = self.generate_difficulty_dropdown_menu()
         self.resolution_dropdown_menu = self.generate_resolution_dropdown_menu()
         self.confirm_button = self.generate_confirm_button()
+        self.add_song_button = self.generate_add_song_button()
 
     # Создание списка кнопок
-    def generate_song_button_array(self):
+    def generate_song_button_array(self, text=None):
+        texts = self.song_list if not text else text
         width = 500 * (screen_width / 1920)
-        height = (60 * (screen_height / 1080) + 30 * (screen_height / 1080)) * len(self.song_list)
+        height = (60 * (screen_height / 1080) + 30 * (screen_height / 1080)) * len(texts)
         song_button_array = NewButtonArray(
             screen,
             int(screen_width // 2 - width // 2),
             int(150 * (screen_height / 1080)),
             int(width),
             int(height),
-            (1, len(self.song_list)),
+            (1, len(texts)),
             border=30 * (screen_height / 1080),
             topBorder=0,
             bottomBorder=0,
             leftBorder=0,
             rightBorder=0,
-            inactiveColours=[(77, 50, 145) for _ in range(len(self.song_list))],
-            hoverColours=[(54, 35, 103) for _ in range(len(self.song_list))],
-            pressedColours=[(121, 78, 230) for _ in range(len(self.song_list))],
+            inactiveColours=[(77, 50, 145) for _ in range(len(texts))],
+            hoverColours=[(54, 35, 103) for _ in range(len(texts))],
+            pressedColours=[(121, 78, 230) for _ in range(len(texts))],
             radii=[25 for _ in range(len(self.song_list))],
-            fonts=[self.buttons_font for _ in range(len(self.song_list))],
-            texts=self.song_list,
+            fonts=[self.buttons_font for _ in range(len(texts))],
+            texts=[t if len(t) < 30 else t[:31] for t in texts],
             colour=(255, 255, 255),
             invisible=True,
-            textColours=[self.buttons_font_color for _ in range(len(self.song_list))],
-            onClicks=[lambda x: self.get_song_name(x) for _ in range(len(self.song_list))],
-            onClickParams=[[i] for i in range(len(self.song_list))]
+            textColours=[self.buttons_font_color for _ in range(len(texts))],
+            onClicks=[lambda x: self.get_song_name(x) for _ in range(len(texts))],
+            onClickParams=[[i] for i in range(len(texts))]
         )
         return song_button_array
 
     # Создание списка сложностей
     def generate_difficulty_dropdown_menu(self):
         difficulty_drop_menu = Dropdown(
-            screen, 0, 0, int(200 * (screen_width / 1920)), int(50 * (screen_width / 1920)), name='difficult',
+            screen, 0, 0, int(200 * (screen_width / 1920)), int(50 * (screen_height / 1080)), name='difficult',
             choices=[
                 'normal',
                 'medium',
@@ -132,7 +133,7 @@ class Menu:
         possible_resolutions = possible_resolutions[:possible_resolutions.index((max_w, max_h)) + 1]
         resolution_drop_menu = Dropdown(
             screen, screen_width - int(200 * (screen_width / 1920)), 0, int(200 * (screen_width / 1920)),
-            int(50 * (screen_width / 1920)), name='resolution',
+            int(50 * (screen_height / 1080)), name='resolution',
             values=possible_resolutions,
             choices=[f'{resolution[0]}*{resolution[1]}' for resolution in possible_resolutions],
             borderRadius=25, direction='down', textHAlign='centre',
@@ -149,7 +150,7 @@ class Menu:
         fps_drop_menu = Dropdown(
             screen, int(screen_width - 200 * (screen_width / 1920) * 2 - (30 * (screen_width / 1920))),
             0, int(200 * (screen_width / 1920)),
-            int(50 * (screen_width / 1920)), name='frame rate',
+            int(50 * (screen_height / 1080)), name='frame rate',
             choices=['30', '60', '120', '144', '240'],
             borderRadius=25, direction='down', textHAlign='centre',
             font=self.buttons_font,
@@ -175,17 +176,36 @@ class Menu:
     # Обновление всех виджетов
     def update_widgets(self, event):
         pygame_widgets.update(event)
+        self.scroll_song_buttons(event)
+
+    # Промотка кнопок песен на колесико мыши
+    def scroll_song_buttons(self, events):
+        pos = pygame.mouse.get_pos()
+        if not pygame.Rect(self.buttonArray.getX(), self.buttonArray.getY(), self.buttonArray.getWidth(),
+                           screen_height).collidepoint(pos):
+            return
+        for event in events:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                WidgetHandler.removeWidget(self.buttonArray)
+                if event.button == 4:
+                    if self.buttons_scroll < len(self.song_list) - 1:
+                        self.buttons_scroll += 1
+                elif event.button == 5:
+                    if self.buttons_scroll > 0:
+                        self.buttons_scroll -= 1
+                self.buttonArray = self.generate_song_button_array(text=self.song_list[self.buttons_scroll:])
 
     # Измение под новое разрешение экрана
     def refactor(self, resolution):
         global screen_width, screen_height, screen
         screen_width, screen_height = resolution
-        screen = pygame.display.set_mode(resolution)
+        screen = pygame.display.set_mode((screen_width, screen_height))
         WidgetHandler.removeWidget(self.buttonArray)
         WidgetHandler.removeWidget(self.FPS_dropdown_menu)
         WidgetHandler.removeWidget(self.difficulty_dropdown_menu)
         WidgetHandler.removeWidget(self.resolution_dropdown_menu)
         WidgetHandler.removeWidget(self.confirm_button)
+        WidgetHandler.removeWidget(self.add_song_button)
         self.buttons_font = pygame.font.Font('materials\\Press Start 2P.ttf', int(15 * (screen_width / 1920)))
         self.buttonArray = self.generate_song_button_array()
         self.FPS_dropdown_menu = self.generate_fps_dropdown_menu()
@@ -193,15 +213,16 @@ class Menu:
         self.resolution_dropdown_menu = self.generate_resolution_dropdown_menu()
         self.confirm_button = self.generate_confirm_button()
         self.menu_image = pygame.transform.smoothscale(self.menu_image, (screen_width, screen_height))
+        self.add_song_button = self.generate_add_song_button()
 
     # создание кнопки подтверждения настроек
     def generate_confirm_button(self):
         confirm_button = Button(
             screen,
             screen_width - int(200 * (screen_width / 1920)),
-            screen_height - int(50 * (screen_width / 1920)),
+            screen_height - int(50 * (screen_height / 1080)),
             int(200 * (screen_width / 1920)),
-            int(50 * (screen_width / 1920)),
+            int(50 * (screen_height / 1080)),
             text='confirm',
             radius=25,
             textColour=self.buttons_font_color,
@@ -218,10 +239,29 @@ class Menu:
         global fps, screen_width, screen_height
         new_fps = self.FPS_dropdown_menu.getSelected()
         new_resolution = self.resolution_dropdown_menu.getSelected()
+        self.FPS_dropdown_menu.reset()
         if new_fps:
             fps = int(new_fps)
         if new_resolution and new_resolution != (screen_width, screen_height):
             self.refactor(new_resolution)
+
+    def generate_add_song_button(self):
+        add_button = Button(
+            screen,
+            screen_width // 2 - int(37.5 * (screen_width / 1920)),
+            screen_height - int(100 * (screen_height / 1080)),
+            int(75 * (screen_width / 1920)),
+            int(75 * (screen_height / 1080)),
+            text='+',
+            font=self.buttons_font,
+            textColour=self.buttons_font_color,
+            inactiveColour=(77, 50, 145),
+            hoverColour=(54, 35, 103),
+            pressedColour=(121, 78, 230),
+            radius=75,
+            border=30 * (screen_height / 1080)
+            )
+        return add_button
 
 
 # Основной цикл игры
